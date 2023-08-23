34d9a892a948db8dcd9a00fd8df994752fe1ce29
PedroJuri <p3dr1n018@outlook.com>
2023-08-23 12:49:34 -0300

    feat: remaking a pro CardNews component

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -3,7 +3,47 @@ class CardNews extends HTMLElement {
         super();
 
         const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello Pedro</h1>";
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "cardleft");
+
+        const author = document.createElement("span");
+        author.textContent = "By " + (this.getAttribute("author") || "Anonymous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
+
+        cardLeft.appendChild(author);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+        
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "cardright");
+
+        const newsImage = document.createElement("img");
+        cardRight.appendChild(newsImage);
+        newsImage.src = this.getAttribute("img-src") || "assets/default-profile.png";
+        newsImage.alt = this.getAttribute("img-alt") || "Imagem";
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    styles(){
+
     }
 }
 
